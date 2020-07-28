9555ba295ba4579116a631b7f167a0fb9917d732
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-07-28 15:46:42 +0300

    Allure added

--- src/test/java/ru/netology/rest/DataClass.java
+++ src/test/java/ru/netology/rest/DataClass.java
@@ -7,16 +7,13 @@ import java.util.*;
 public class DataClass {
     DataClass() {}
 
-    private enum Cities {
-            Москва, Санкт_Петербург, Казань, Воронеж, Волгоград, Екатеринбург, Саратов, Новосибирск, Владикавказ, Владивосток, Ярославль, Краснодар;
+    public static String getCity() {
+        Random rand = new Random();
+        List<String> list = Arrays.asList("Москва", "Санкт-Петербург", "Казань", "Воронеж", "Волгоград", "Екатеринбург", "Саратов", "Новосибирск", "Владикавказ", "Владивосток", "Ярославль", "Краснодар");
+        int randomIndex = rand.nextInt(list.size());
+        String randomElement = list.get(randomIndex);
+        return randomElement;
     }
-            private static final List<Cities> values = Collections.unmodifiableList(Arrays.asList(Cities.values()));
-            private static final int size = values.size();
-            private static final Random random = new Random(11);
-            public static String randomCity() {
-            return values.get(random.nextInt(size)).toString();
-            }
-
 
     public static String getName() {
         Faker faker = new Faker(new Locale("ru"));
--- src/test/java/ru/netology/rest/FormTest.java
+++ src/test/java/ru/netology/rest/FormTest.java
@@ -2,6 +2,10 @@ package ru.netology.rest;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -12,72 +16,82 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.rest.DataClass.*;
 
 
 public class FormTest {
-    private DataClass dataClass = new DataClass();
-     String City= dataClass.randomCity();
-     String Name = dataClass.getName();
-     String Phone = dataClass.getPhone();
+    String City = getCity();
+    String Name = getName();
+    String Phone = getPhone();
     LocalDate data = LocalDate.now().plusDays(5);
     DateTimeFormatter DateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     LocalDate now = LocalDate.now();
 
-    @Test
-    void shouldRegistrationForm(){
-        open("http://localhost:9999");
-        SelenideElement form = $("[action]");
-        form.$(("[data-test-id=city] input")).sendKeys(City);
-        form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
-        form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
-        form.$(("[name=name]")).sendKeys(Name);
-        form.$(("[name=phone]")).sendKeys(Phone);
-        form.$(("[data-test-id=agreement]")).click();
-        form.$(byText("Запланировать")).click();
-        $((".notification__title")).waitUntil(Condition.visible, 15000);
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
-    @Test
-    void shouldRegistrationNewDateCheck() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[action]");
-        form.$(("[data-test-id=city] input")).sendKeys(City);
-        form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
-        form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
-        form.$(("[name=name]")).sendKeys(Name);
-        form.$(("[name=phone]")).sendKeys(Phone);
-        form.$(("[data-test-id=agreement]")).click();
-        form.$(byText("Запланировать")).click();
-        $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
-        form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
-        form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
-        form.$(byText("Запланировать")).click();
-        $((".notification_status_error .button")).click();
-        $(withText("Успешно!")).waitUntil(Condition.visible, 15000);
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
-    @Test
-    void shouldNotRegistrationIfDataInvalid() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[action]");
-        form.$(("[data-test-id=city] input")).sendKeys(City);
-        form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
-        form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
-        form.$(("[name=name]")).sendKeys(Name);
-        form.$(("[name=phone]")).sendKeys(Phone);
-        form.$(("[data-test-id=agreement]")).click();
-        form.$(byText("Запланировать")).click();
-        $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
-        form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
-        form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
-        form.$(("[data-test-id=date] input")).sendKeys(now.format(DateFormatter));
-        form.$(("[data-test-id=agreement]")).click();
-        form.$(byText("Запланировать")).click();
-        $(byText("Заказ на выбранную дату невозможен")).waitUntil(Condition.visible, 15000);
-    }
+        @Test
+        void shouldRegistrationForm () {
+            open("http://localhost:9999");
+            SelenideElement form = $("[action]");
+            form.$(("[data-test-id=city] input")).sendKeys(City);
+            form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+            form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
+            form.$(("[name=name]")).sendKeys(Name);
+            form.$(("[name=phone]")).sendKeys(Phone);
+            form.$(("[data-test-id=agreement]")).click();
+            form.$(byText("Запланировать")).click();
+            $((".notification__title")).waitUntil(Condition.visible, 15000);
+        }
 
-}
+        @Test
+        void shouldRegistrationNewDateCheck () {
+            open("http://localhost:9999");
+            SelenideElement form = $("[action]");
+            form.$(("[data-test-id=city] input")).sendKeys(City);
+            form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+            form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
+            form.$(("[name=name]")).sendKeys(Name);
+            form.$(("[name=phone]")).sendKeys(Phone);
+            form.$(("[data-test-id=agreement]")).click();
+            form.$(byText("Запланировать")).click();
+            $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
+            form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+            form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
+            form.$(byText("Запланировать")).click();
+            $((".notification_status_error .button")).click();
+            $(withText("Успешно!")).waitUntil(Condition.visible, 15000);
+        }
+
+        @Test
+        void shouldNotRegistrationIfDataInvalid () {
+            open("http://localhost:9999");
+            SelenideElement form = $("[action]");
+            form.$(("[data-test-id=city] input")).sendKeys(City);
+            form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+            form.$(("[data-test-id=date] input")).sendKeys(data.format(DateFormatter));
+            form.$(("[name=name]")).sendKeys(Name);
+            form.$(("[name=phone]")).sendKeys(Phone);
+            form.$(("[data-test-id=agreement]")).click();
+            form.$(byText("Запланировать")).click();
+            $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
+            form.$(("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+            form.$(("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+            form.$(("[data-test-id=date] input")).sendKeys(now.format(DateFormatter));
+            form.$(("[data-test-id=agreement]")).click();
+            form.$(byText("Запланировать")).click();
+            $(byText("Заказ на выбранную дату невозможен")).waitUntil(Condition.visible, 15000);
+        }
+
+    }
